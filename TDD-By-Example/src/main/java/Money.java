2f840fb3b0e18a2ccb161c337ffe8983a39b42be
JeonJoonHo <jeonjh9@naver.com>
2021-01-20 23:00:27 +0900

    TDD: By Example : Chapter 9. 우리가 사는 시간

--- TDD-By-Example/src/main/java/Money.java
+++ TDD-By-Example/src/main/java/Money.java
@@ -1,15 +1,25 @@
 abstract class Money {
     protected int amount;
-
-    abstract Money times(int multiplier);
+    protected String currency;
 
     static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    String currency() {
+        return currency;
+    }
+
+    abstract Money times(int multiplier);
 
     public boolean equals(Object object) {
         Money money = (Money) object;
